82df5ec2f540dcadc24fcacd152aac28c18fe8fe
juanluis.garciar <juanluis.garciar@juntaex.es>
2023-03-17 12:32:14 +0100

    problema109

--- src/problema_109/LigaPadel.java
+++ src/problema_109/LigaPadel.java
@@ -5,36 +5,28 @@ import java.util.Scanner;
 
 public class LigaPadel {
 
-    private static class Partido{
-        private String ganador;
-        private String perdedor;
 
-        protected Partido(String _entradaPartido){
-            String[] entrada = _entradaPartido.split(" ");
-            if ( Byte.parseByte(entrada[1]) > Byte.parseByte(entrada[3]) ) {
-                ganador = entrada[0];
-                perdedor = entrada[2];
-            } else {
-                ganador = entrada[2];
-                perdedor = entrada[0];
-            }
-        }
-
-        protected String getGanador(){
-            return ganador;
-        }
-
-        protected String getPerdedor(){
-            return perdedor;
-        }
-    }
     public static void main(String[] args){
         String[] entrada;
+
 
         Scanner sc = new Scanner(System.in).useDelimiter("\n");
         sc.useLocale(Locale.UK);
         while( sc.hasNext() ){
             entrada = sc.next().split(" ");
+            if ( entrada.length == 1 ){
+                // categoría
+            } else {
+                // partido
+                if ( Byte.parseByte(entrada[1]) > Byte.parseByte(entrada[3]) ) {
+                    //ganador = entrada[0];
+                    //perdedor = entrada[2];
+                } else {
+                    //ganador = entrada[2];
+                    //perdedor = entrada[0];
+                }
+            }
+            }
         }
         sc.close();
         System.exit(0);
